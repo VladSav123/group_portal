1b941a74d7605ee97d2c100a3db705a7bdd91424
sashapre228 <sasha291108@gmail.com>
2024-05-11 14:37:20 +0200

    lesson

--- group_portal/settings.py
+++ group_portal/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 	'gallery',
 	'auth_system',
 	'main_app',
+	'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -64,7 +65,7 @@ ROOT_URLCONF = 'group_portal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,8 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+	BASE_DIR / 'static'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "auth_system.CustomUser"
